42a7e295444a9dcf9eb235b430993e4be99e6bcc
Ghughe <ptp112004@gmail.com>
2023-01-28 22:00:15 +0700

    second chagne

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Welcome to our homepage");
-  }
-  if (req.url === "/about") {
-    res.end("Here is our short history");
-  }
-  res.end(`<h1> Opps </h1>
-    <a href = '/'>Back to home page </a>`);
-});
-server.listen(8000);
+console.log("Welcome to Node");
