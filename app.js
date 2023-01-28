ae764c57bfd7f3d8e9738981f18f51cb30665db3
Ghughe <ptp112004@gmail.com>
2023-01-28 22:01:20 +0700

    second chagne

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Welcome to Node");
+const { readFile } = require("fs");
+console.log("Stared a first task");
+readFile("./content/firstfile.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("completed first task");
+});
+console.log("Starting next task");
